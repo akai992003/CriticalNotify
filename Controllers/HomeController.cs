ab7cea235fef998091b92c4b422b23e17b078591
akai <akai@single.url.com.tw>
2022-06-13 17:19:06 +0800

    111.06.13

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 using CriticalNotify.Data;
 using CriticalNotify.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Linq;
 namespace CriticalNotify.Controllers;
 
 public class HomeController : Controller
@@ -29,8 +29,11 @@ public class HomeController : Controller
 
     public async Task<IActionResult> Privacy()
     {
-        var q = await I繳費.取得拆帳字串("1110601");
+        var q = await I繳費.取得拆帳字串("1110526");
+        var q2 = await I繳費.取得拆帳字串("1110526","體一");
         var dl = new List<dto拆帳字串>();
+        var dl2 = new List<dto拆帳字串>();
+        var mo = 0;
         foreach (var p in q)
         {
             if (p != "" || p != null)
@@ -41,32 +44,166 @@ public class HomeController : Controller
 
                 // }
                 var dto = new dto拆帳字串();
-                dto.診察費 = double.Parse(ps[0]);
-                dto.病房費 = double.Parse(ps[1]);
-                dto.管灌膳食費 = double.Parse(ps[2]);
-                dto.檢查費 = double.Parse(ps[3]);
-                dto.放射線診療費 = double.Parse(ps[4]);
-                dto.治療處置費 = double.Parse(ps[5]);
-                dto.手術費 = double.Parse(ps[6]);
-                dto.復健治療費 = double.Parse(ps[7]);
-                dto.血液血漿費 = double.Parse(ps[8]);
-                dto.血液透析費 = double.Parse(ps[9]);
-                dto.麻醉費 = double.Parse(ps[10]);
-                dto.特殊材料費 = double.Parse(ps[11]);
-                dto.藥費 = double.Parse(ps[12]);
-                dto.藥事服務費 = double.Parse(ps[13]);
-                dto.精神科治療費 = double.Parse(ps[14]);
-                dto.注射技術費 = double.Parse(ps[15]);
-                dto.嬰兒費 = double.Parse(ps[16]);
-                dto.自定 = double.Parse(ps[17]);
-                dto.檢驗費 = double.Parse(ps[18]);
-                dto.工本費 = double.Parse(ps[19]);
-                dto.手續費 = double.Parse(ps[20]);
 
+                dto._01病房費 = double.Parse(ps[2]);
+                dto._02膳食費 = double.Parse(ps[3]);
+                dto._03藥品費 = double.Parse(ps[4]);
+                dto._04檢驗費 = double.Parse(ps[5]);
+                dto._05心電圖費 = double.Parse(ps[6]);
+                dto._06X光費 = double.Parse(ps[7]);
+                dto._07X光材料費 = double.Parse(ps[8]);
+                dto._08醫療衛材費 = double.Parse(ps[9]);
+                dto._09治療費 = double.Parse(ps[10]);
+                dto._10手術房費 = double.Parse(ps[11]);
+                dto._11麻醉藥品費 = double.Parse(ps[12]);
+                dto._12助理醫師費 = double.Parse(ps[13]);
+                dto._13護理費 = double.Parse(ps[14]);
+                dto._14洗腎費 = double.Parse(ps[15]);
+                dto._15證明書費 = double.Parse(ps[16]);
+                dto._16復健治療費 = double.Parse(ps[17]);
+                dto._17育嬰費 = double.Parse(ps[18]);
+                dto._18斷層攝影費 = double.Parse(ps[19]);
+                dto._19本院助手費 = double.Parse(ps[20]);
+                dto._20核子檢驗費 = double.Parse(ps[21]);
+                dto._21切片檢查費 = double.Parse(ps[22]);
+                dto._22尿動力測驗 = double.Parse(ps[23]);
+                dto._23電話費 = double.Parse(ps[24]);
+                dto._24手續費 = double.Parse(ps[25]);
+                dto._25一般材料費 = double.Parse(ps[26]);
+                dto._26病歷摘要 = double.Parse(ps[27]);
+                dto._27檢驗材料費 = double.Parse(ps[28]);
+                dto._28手術材料費 = double.Parse(ps[29]);
+                dto._29 = double.Parse(ps[30]);
+                dto._30掛號費 = double.Parse(ps[31]);
+                dto._31診察費 = double.Parse(ps[32]);
+                dto._32門診會診費 = double.Parse(ps[33]);
+                dto._33住院會診費 = double.Parse(ps[34]);
+                dto._34手術費 = double.Parse(ps[35]);
+                dto._35麻醉費 = double.Parse(ps[36]);
+                dto._36助手費 = double.Parse(ps[37]);
+                dto._37加護病房費 = double.Parse(ps[38]);
+                dto._38眼科技術費 = double.Parse(ps[39]);
+                dto._39特別護士費 = double.Parse(ps[40]);
+                dto._40特別材料費 = double.Parse(ps[41]);
+                dto._41技術治療費 = double.Parse(ps[42]);
+                dto._42血片判讀費 = double.Parse(ps[43]);
+                dto._43健保門診自付款 = double.Parse(ps[44]);
+                dto._44健保住診自付款 = double.Parse(ps[45]);
+                dto._45藥事服務費 = double.Parse(ps[46]);
+                dto._46初診工本費 = double.Parse(ps[47]);
+                dto._47影印費 = double.Parse(ps[48]);
+                dto._48衛教費 = double.Parse(ps[49]);
+                dto._49指定醫師費 = double.Parse(ps[50]);
+                dto._50醫療諮詢費 = double.Parse(ps[51]);
+                dto._51輔具費 = double.Parse(ps[52]);
+                dto._52檢查費 = double.Parse(ps[53]);
+                dto._53精神科鑑定費 = double.Parse(ps[54]);
+                dto._54實習費 = double.Parse(ps[55]);
+                dto._55醫事服務費 = double.Parse(ps[56]);
+                dto._56醫美技術治療費 = double.Parse(ps[57]);
+                dto._57營養配方 = double.Parse(ps[58]);
+                dto._58處置費 = double.Parse(ps[59]);
+                dto._59 = double.Parse(ps[60]);
+                dto._60健康檢查 = double.Parse(ps[61]);
+                dto._61影印費 = double.Parse(ps[62]);
+                dto._62病情諮詢費 = double.Parse(ps[63]);
+                dto._63保險公司指定醫師費 = double.Parse(ps[64]);
+                dto._64頭皮諮詢 = double.Parse(ps[65]);
+                dto._65疼痛麻醉費 = double.Parse(ps[66]);
+                dto._66疼痛麻醉材料費 = double.Parse(ps[67]);
+                dto._67無痛鏡檢麻醉費 = double.Parse(ps[68]);
+                dto._68無痛鏡檢材料費 = double.Parse(ps[69]);
                 dl.Add(dto);
             }
 
         }
+        foreach (var p in q2)
+        {
+            if (p != "" || p != null)
+            {
+                var ps = p.Split(";");
+                // for (int i = 0; i < ps.Length; i++)
+                // {
+
+                // }
+                var dto = new dto拆帳字串();
+
+                dto._01病房費 = double.Parse(ps[2]);
+                dto._02膳食費 = double.Parse(ps[3]);
+                dto._03藥品費 = double.Parse(ps[4]);
+                dto._04檢驗費 = double.Parse(ps[5]);
+                dto._05心電圖費 = double.Parse(ps[6]);
+                dto._06X光費 = double.Parse(ps[7]);
+                dto._07X光材料費 = double.Parse(ps[8]);
+                dto._08醫療衛材費 = double.Parse(ps[9]);
+                dto._09治療費 = double.Parse(ps[10]);
+                dto._10手術房費 = double.Parse(ps[11]);
+                dto._11麻醉藥品費 = double.Parse(ps[12]);
+                dto._12助理醫師費 = double.Parse(ps[13]);
+                dto._13護理費 = double.Parse(ps[14]);
+                dto._14洗腎費 = double.Parse(ps[15]);
+                dto._15證明書費 = double.Parse(ps[16]);
+                dto._16復健治療費 = double.Parse(ps[17]);
+                dto._17育嬰費 = double.Parse(ps[18]);
+                dto._18斷層攝影費 = double.Parse(ps[19]);
+                dto._19本院助手費 = double.Parse(ps[20]);
+                dto._20核子檢驗費 = double.Parse(ps[21]);
+                dto._21切片檢查費 = double.Parse(ps[22]);
+                dto._22尿動力測驗 = double.Parse(ps[23]);
+                dto._23電話費 = double.Parse(ps[24]);
+                dto._24手續費 = double.Parse(ps[25]);
+                dto._25一般材料費 = double.Parse(ps[26]);
+                dto._26病歷摘要 = double.Parse(ps[27]);
+                dto._27檢驗材料費 = double.Parse(ps[28]);
+                dto._28手術材料費 = double.Parse(ps[29]);
+                dto._29 = double.Parse(ps[30]);
+                dto._30掛號費 = double.Parse(ps[31]);
+                dto._31診察費 = double.Parse(ps[32]);
+                dto._32門診會診費 = double.Parse(ps[33]);
+                dto._33住院會診費 = double.Parse(ps[34]);
+                dto._34手術費 = double.Parse(ps[35]);
+                dto._35麻醉費 = double.Parse(ps[36]);
+                dto._36助手費 = double.Parse(ps[37]);
+                dto._37加護病房費 = double.Parse(ps[38]);
+                dto._38眼科技術費 = double.Parse(ps[39]);
+                dto._39特別護士費 = double.Parse(ps[40]);
+                dto._40特別材料費 = double.Parse(ps[41]);
+                dto._41技術治療費 = double.Parse(ps[42]);
+                dto._42血片判讀費 = double.Parse(ps[43]);
+                dto._43健保門診自付款 = double.Parse(ps[44]);
+                dto._44健保住診自付款 = double.Parse(ps[45]);
+                dto._45藥事服務費 = double.Parse(ps[46]);
+                dto._46初診工本費 = double.Parse(ps[47]);
+                dto._47影印費 = double.Parse(ps[48]);
+                dto._48衛教費 = double.Parse(ps[49]);
+                dto._49指定醫師費 = double.Parse(ps[50]);
+                dto._50醫療諮詢費 = double.Parse(ps[51]);
+                dto._51輔具費 = double.Parse(ps[52]);
+                dto._52檢查費 = double.Parse(ps[53]);
+                dto._53精神科鑑定費 = double.Parse(ps[54]);
+                dto._54實習費 = double.Parse(ps[55]);
+                dto._55醫事服務費 = double.Parse(ps[56]);
+                dto._56醫美技術治療費 = double.Parse(ps[57]);
+                dto._57營養配方 = double.Parse(ps[58]);
+                dto._58處置費 = double.Parse(ps[59]);
+                dto._59 = double.Parse(ps[60]);
+                dto._60健康檢查 = double.Parse(ps[61]);
+                dto._61影印費 = double.Parse(ps[62]);
+                dto._62病情諮詢費 = double.Parse(ps[63]);
+                dto._63保險公司指定醫師費 = double.Parse(ps[64]);
+                dto._64頭皮諮詢 = double.Parse(ps[65]);
+                dto._65疼痛麻醉費 = double.Parse(ps[66]);
+                dto._66疼痛麻醉材料費 = double.Parse(ps[67]);
+                dto._67無痛鏡檢麻醉費 = double.Parse(ps[68]);
+                dto._68無痛鏡檢材料費 = double.Parse(ps[69]);
+                dl2.Add(dto);
+            }
+
+        }
+        var total檢驗費 = dl.Sum(c => c._04檢驗費);
+        var total檢驗費體一 = dl2.Sum(d => d._04檢驗費);
+        ViewBag.total檢驗費 = total檢驗費;
+        ViewBag.total檢驗費體一 = total檢驗費體一;
         return View(dl);
     }
 
--- Data/繳費檔.cs
+++ Data/繳費檔.cs
@@ -7,40 +7,92 @@ public class 繳費檔
 {
     [Key]
     public Int32 counter { get; set; }
+    public Int32 病患檔_counter { get; set; }
     public string 拆帳字串 { get; set; }
-    public string 繳費日期 { get; set; }
-
-
+    public string 日期 { get; set; }
+    public string 經手人代號 { get; set; }
+    public string 註銷否 { get; set; }
+    public string 醫師代號 { get; set; }
+    public string 付款方式代碼 { get; set; }
 }
 
 public class dto拆帳字串
 {
-    public double 診察費 { get; set; }
-    public double 病房費 { get; set; }
-    public double 管灌膳食費 { get; set; }
-    public double 檢查費 { get; set; }
-    public double 放射線診療費 { get; set; }
-    public double 治療處置費 { get; set; }
-    public double 手術費 { get; set; }
-    public double 復健治療費 { get; set; }
-    public double 血液血漿費 { get; set; }
-    public double 血液透析費 { get; set; }
-    public double 麻醉費 { get; set; }
-    public double 特殊材料費 { get; set; }
-    public double 藥費 { get; set; }
-    public double 藥事服務費 { get; set; }
-    public double 精神科治療費 { get; set; }
-    public double 注射技術費 { get; set; }
-    public double 嬰兒費 { get; set; }
-    public double 自定 { get; set; }
-    public double 檢驗費 { get; set; }
-    public double 工本費 { get; set; }
-    public double 手續費 { get; set; }
+
+    public double _01病房費 { get; set; }
+    public double _02膳食費 { get; set; }
+    public double _03藥品費 { get; set; }
+    public double _04檢驗費 { get; set; }
+    public double _05心電圖費 { get; set; }
+    public double _06X光費 { get; set; }
+    public double _07X光材料費 { get; set; }
+    public double _08醫療衛材費 { get; set; }
+    public double _09治療費 { get; set; }
+    public double _10手術房費 { get; set; }
+    public double _11麻醉藥品費 { get; set; }
+    public double _12助理醫師費 { get; set; }
+    public double _13護理費 { get; set; }
+    public double _14洗腎費 { get; set; }
+    public double _15證明書費 { get; set; }
+    public double _16復健治療費 { get; set; }
+    public double _17育嬰費 { get; set; }
+    public double _18斷層攝影費 { get; set; }
+    public double _19本院助手費 { get; set; }
+    public double _20核子檢驗費 { get; set; }
+    public double _21切片檢查費 { get; set; }
+    public double _22尿動力測驗 { get; set; }
+    public double _23電話費 { get; set; }
+    public double _24手續費 { get; set; }
+    public double _25一般材料費 { get; set; }
+    public double _26病歷摘要 { get; set; }
+    public double _27檢驗材料費 { get; set; }
+    public double _28手術材料費 { get; set; }
+    public double _29 { get; set; }
+    public double _30掛號費 { get; set; }
+    public double _31診察費 { get; set; }
+    public double _32門診會診費 { get; set; }
+    public double _33住院會診費 { get; set; }
+    public double _34手術費 { get; set; }
+    public double _35麻醉費 { get; set; }
+    public double _36助手費 { get; set; }
+    public double _37加護病房費 { get; set; }
+    public double _38眼科技術費 { get; set; }
+    public double _39特別護士費 { get; set; }
+    public double _40特別材料費 { get; set; }
+    public double _41技術治療費 { get; set; }
+    public double _42血片判讀費 { get; set; }
+    public double _43健保門診自付款 { get; set; }
+    public double _44健保住診自付款 { get; set; }
+    public double _45藥事服務費 { get; set; }
+    public double _46初診工本費 { get; set; }
+    public double _47影印費 { get; set; }
+    public double _48衛教費 { get; set; }
+    public double _49指定醫師費 { get; set; }
+    public double _50醫療諮詢費 { get; set; }
+    public double _51輔具費 { get; set; }
+    public double _52檢查費 { get; set; }
+    public double _53精神科鑑定費 { get; set; }
+    public double _54實習費 { get; set; }
+    public double _55醫事服務費 { get; set; }
+    public double _56醫美技術治療費 { get; set; }
+    public double _57營養配方 { get; set; }
+    public double _58處置費 { get; set; }
+    public double _59 { get; set; }
+    public double _60健康檢查 { get; set; }
+    public double _61影印費 { get; set; }
+    public double _62病情諮詢費 { get; set; }
+    public double _63保險公司指定醫師費 { get; set; }
+    public double _64頭皮諮詢 { get; set; }
+    public double _65疼痛麻醉費 { get; set; }
+    public double _66疼痛麻醉材料費 { get; set; }
+    public double _67無痛鏡檢麻醉費 { get; set; }
+    public double _68無痛鏡檢材料費 { get; set; }
 }
 
 public interface I繳費Service
 {
     Task<List<string>> 取得拆帳字串(string 日期);
+    Task<List<string>> 取得拆帳字串(string 日期, string 費用單位);
 }
 
 public class 繳費Service : I繳費Service
@@ -51,11 +103,29 @@ public class 繳費Service : I繳費Service
     {
         using var context = new HNContext();
         var q = await (from p in context.繳費檔
-                       where 
+                       where
                        //p.counter == 4923751
-                       p.拆帳字串 != null && p.拆帳字串 != ""
-                       &&  p.繳費日期 == 日期
-                        
+                       p.拆帳字串 != null && p.拆帳字串 != "" && p.付款方式代碼 != "1"
+                       && p.日期 == 日期 && 
+                       (p.經手人代號.Contains("A11784") || p.經手人代號.Contains("A12240") || p.經手人代號.Contains("A12364")
+                       || p.經手人代號.Contains("A12551") || p.經手人代號.Contains("A12433") || p.經手人代號.Contains("A0681") || p.經手人代號.Contains("A12432"))
+                       && p.註銷否 == "N"
+
+                       select p.拆帳字串).ToListAsync();
+        return q;
+
+    }
+    public async Task<List<string>> 取得拆帳字串(string 日期, string 費用單位)
+    {
+        using var context = new HNContext();
+        var q = await (from p in context.繳費檔
+                       where
+                       //p.counter == 4923751
+                       p.拆帳字串 != null && p.拆帳字串 != "" && p.付款方式代碼 != "1"
+                       && p.日期 == 日期 && (p.經手人代號.Contains("A11784") || p.經手人代號.Contains("A12240") || p.經手人代號.Contains("A12364")
+                       || p.經手人代號.Contains("A12551") || p.經手人代號.Contains("A12433") || p.經手人代號.Contains("A0681") || p.經手人代號.Contains("A12432"))
+                       && p.註銷否 == "N" && (p.醫師代號.Contains("ZZ1") || p.醫師代號.Contains("360"))
+                       // A11784=魏素華 ; A12240=袁小媛 ; A12364=蕭蕙華 ; A12551=張蘊禮 ; A12433=謝鴻豪 ; A0681=卓正玲 ; A12432=劉家蓉
                        select p.拆帳字串).ToListAsync();
         return q;
 
